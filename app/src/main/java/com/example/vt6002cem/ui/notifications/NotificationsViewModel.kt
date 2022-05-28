208c875736159e9a922c045a23c5a590b0196b78
dennisauyeung <40794322+ikouhaha@users.noreply.github.com>
2022-05-28 12:41:30 +0800

    init

---
+++ app/src/main/java/com/example/vt6002cem/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.vt6002cem.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
